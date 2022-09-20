886e004e2fa071c35c2e2629a58ec42da92db24d
Khaled Zaza <khaledzaza.cs@gmail.com>
2022-09-20 15:05:14 -0700

    Added ArrayList instead of array because wanted it to be flexible to file sizes

--- src/sjsu/edu/Main.java
+++ src/sjsu/edu/Main.java
@@ -2,7 +2,7 @@ package sjsu.edu;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,25 +10,27 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new File("src/sjsu/edu/arr2-1.txt"));
-        int [] arr2 = new int [50];
-        int x = 0;
+
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
         while(scanner.hasNextInt()){
-            arr2[x++] = scanner.nextInt();
+            arr2.add(scanner.nextInt());
         }
-        System.out.println(Arrays.toString(arr2));
-        int size = arr2.length;
+
+
+        System.out.println(arr2);
+        int size = arr2.size();
 
         for (int i = size-1; i>=1 ; i--){
             int maxIndex = i;
             for (int j= 0; j<i; j++){
-                if (arr2[j] >= arr2[maxIndex])
+                if (arr2.get(j) >= arr2.get(maxIndex))
                     maxIndex = j;
-                int temp = arr2[i];
-                arr2[i] = arr2[maxIndex];
-                arr2[maxIndex] = temp;
+                int temp = arr2.get(i);
+                arr2.set(i, arr2.get(maxIndex));
+                arr2.set(maxIndex, temp);
             }
         }
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(arr2);
 
 
 
